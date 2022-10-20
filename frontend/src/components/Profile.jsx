fb24be5a58d414b2e98b5d391a4925758d8c9c12
nt500 <91642940+nt500@users.noreply.github.com>
2022-10-19 22:06:39 -0700

    Update Profile.jsx

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,9 +1,26 @@
-const Profile = () => {
-  return (
-    <div>
-      <h1>This is the Profile</h1>
-    </div>
-  );
-}
+import { MdPhone, MdEmail } from 'react-icons/md';
 
-export default Profile;
+function Profile( pets ) {
+    return (
+        <table>
+            <tr>
+                <td>
+                    <h2>{pets.name}</h2>
+                    <p>Joined: {pets.dateCreated}</p>
+                    <p>Age: {pets.age}</p>
+                    <p>{pets.gender}</p>
+                    <p>Breed: {pets.breed}</p>   
+                    <p>Disposition: {pets.disposition}</p>
+                    <p>Location: {pets.location}</p>
+                    <p>Contact: <MdPhone/><MdEmail/></p>
+                    <p>About Me: {pets.description}</p>
+                </td>
+                <td>
+                    <img src={pets.pic} alt={pets.name}/>
+                </td>
+            </tr>
+        </table>
+      );
+    }
+    
+    export default Profile;
