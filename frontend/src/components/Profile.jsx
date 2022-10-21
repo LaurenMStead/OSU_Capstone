9cb2aebf324640eec52762d2d24db889c6b48555
nt500 <91642940+nt500@users.noreply.github.com>
2022-10-20 19:25:36 -0700

    Update Profile.jsx
    Incorporated the logic from ProfilePage.jsx (which I now deleted) into this file, so that Profile.jsx is a single component

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,25 +1,31 @@
 import { MdPhone, MdEmail } from 'react-icons/md';
+import NavBar from '../components/NavBar.js';
 
 function Profile( pets ) {
     return (
-        <table>
-            <tr>
-                <td>
-                    <h2>{pets.name}</h2>
-                    <p>Joined: {pets.dateCreated}</p>
-                    <p>Age: {pets.age}</p>
-                    <p>{pets.gender}</p>
-                    <p>Breed: {pets.breed}</p>   
-                    <p>Disposition: {pets.disposition}</p>
-                    <p>Location: {pets.location}</p>
-                    <p>Contact: <MdPhone/><MdEmail/></p>
-                    <p>About Me: {pets.description}</p>
-                </td>
-                <td>
-                    <img src={pets.pic} alt={pets.name}/>
-                </td>
-            </tr>
-        </table>
+        <div>
+            <NavBar />
+            <br></br>
+            <br></br>
+            <table>
+                <tr>
+                    <td>
+                        <h2>{pets.name}</h2>
+                        <p>Joined: {pets.dateCreated}</p>
+                        <p>Age: {pets.age}</p>
+                        <p>{pets.gender}</p>
+                        <p>Breed: {pets.breed}</p>   
+                        <p>Disposition: {pets.disposition}</p>
+                        <p>Location: {pets.location}</p>
+                        <p>Contact: <MdPhone/><MdEmail/></p>
+                        <p>About Me: {pets.description}</p>
+                    </td>
+                    <td>
+                        <img src={pets.pic} alt={pets.name}/>
+                    </td>
+                </tr>
+            </table>
+        </div>
       );
     }
     
