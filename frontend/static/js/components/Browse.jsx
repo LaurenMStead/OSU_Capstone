756d6b12fa267d347c65b07ed45176b4dcc1659a
Shawn Kim <shawnnicolekim@gmail.com>
2022-10-27 16:47:57 -0700

    Changes to Browse and NavBar

--- frontend/static/js/components/Browse.jsx
+++ frontend/static/js/components/Browse.jsx
@@ -1,9 +1,14 @@
 import ProfileCard from './ProfileCard.jsx';
+import axios from 'axios';
 
 const Browse = ({ selectPet }) => {
+
+  //TO-DO: create HTTP request with axios. GET 1 pet.
+
   return (
     <div>
       <h1>This is the Browse</h1>
+      <ProfileCard selectPet={selectPet} />
     </div>
   );
 }
--- frontend/static/js/components/NavBar.jsx
+++ frontend/static/js/components/NavBar.jsx
@@ -2,13 +2,20 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () =>  {
   // TO-DO: if user is logged in, show logout button. But if user is not logged in, show login button. This might be done in top-level (App.jsx)
+  const isLoggedIn = () => {
+    if (false) {
+      // show logout button
+    } else {
+      return <NavLink id="loginButton" to="/login">Login</NavLink>
+    }
+  }
 
   return (
     <div id="navBar">
       <NavLink id="homeButton" to="/feed">Home</NavLink>
       <NavLink id="searchButton" to="/search">Search</NavLink>
       <NavLink id="browseButton" to="/browse">Browse</NavLink>
-      <NavLink id="loginButton" to="/login">Login</NavLink>
+      {isLoggedIn()}
     </div>
   );
 }
