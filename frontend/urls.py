072490626f32975c492f01ee806cf10fa96a7839
Sebastian Espinas <espinass@oregonstate.edu>
2022-10-28 16:41:52 -0700

    update urls.py for browse, login, profile, search, and signup

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, search, browse, login, signup, profile
 
 urlpatterns = [
     path('', index),            # home page/ news feed
-    path('signup', index),      # signup page
+    path('search', search),      # search page
+    path('browse', browse),      # browse page
+    path('login', login),      # login page
+    path('signup', signup),      # signup page
+    path('profile', profile),      # profile page
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 
 def index(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
+
+def search(request, *args, **kwargs):
+    return render(request, 'frontend/search.html')
+
+def browse(request, *args, **kwargs):
+    return render(request, 'frontend/browse.html')
+
+def login(request, *args, **kwargs):
+    return render(request, 'frontend/login.html')
+
+def signup(request, *args, **kwargs):
+    return render(request, 'frontend/signup.html')
+
+def profile(request, *args, **kwargs):
+    return render(request, 'frontend/profile.html')
+
