1c26a331b41d5f57dca27be4e3f886d77225611d
Lauren Ruff <ruffl@oregonstate.edu>
2022-11-03 16:26:21 -0700

    added defaults to models.py

--- api/models.py
+++ api/models.py
@@ -1,17 +1,4 @@
-import random
-import string
-
 from django.db import models
-
-
-# FOR TESTING PURPOSES ONLY
-def return_rand_code():
-    length = 5
-
-    while True:
-        code = ''.join(random.choices(string.ascii_uppercase, k=length))
-        if Codes.objects.filter(code=code).count() == 0:
-            return code
 
 
 # Create your models here.
@@ -30,38 +17,74 @@ class Types(models.Model):
         (DOG, 'Dog'),
         (CAT, 'Cat'),
         (REPTILE, 'Reptile'),
-        (OTHER, 'Other')
+        (OTHER, 'Other'),
     ]
-    type = models.CharField(max_length=25, choices=PET_TYPE_CHOICES)
+    type = models.CharField(max_length=25, choices=PET_TYPE_CHOICES, blank=False)
 
 
 class Breeds(models.Model):
-    breed = models.CharField(max_length=50)
+    PET_BREED_CHOICES = [
+        ('Poodle', 'Poodle'),
+        ('German Shepherd', 'German Shepherd'),
+        ('Chihuahua', 'Chihuahua'),
+        ('Mut', 'Mut'),
+        ('Labrador', 'Labrador'),
+        ('DSH', 'Domestic Short Hair'),
+        ('DLH', 'Domestic Long Hair'),
+        ('Siamese', 'Siamese'),
+        ('Russian Blue', 'Russian Blue'),
+        ('Maine Coon', 'Maine Coon'),
+        ('Fish', 'Fish'),
+        ('Snake', 'Snake'),
+        ('Turtle', 'Turtle'),
+        ('Iguana', 'Iguana'),
+    ]
+    breed = models.CharField(max_length=50, choices=PET_BREED_CHOICES, blank=True)
 
 
 class Ages(models.Model):
+    BABY = 'B'
+    YOUNG = 'Y'
+    ADULT = 'A',
+    SENIOR = 'S'
     PET_AGE_CHOICES = [
         ('Baby', 'Baby'),
         ('Young', 'Young'),
         ('Adult', 'Adult'),
-        ('Senior', 'Senior')
+        ('Senior', 'Senior'),
     ]
-    age = models.CharField(max_length=25, choices=PET_AGE_CHOICES)
+    age = models.CharField(max_length=25, choices=PET_AGE_CHOICES, blank=False)
 
 
 class Dispositions(models.Model):
+
+    OPTION_1 = 'Good with children'
+    OPTION_2 = 'Good with other animals'
+    OPTION_3 = 'Must be leashed at all times'
+
     PET_DISPOSITION_CHOICES = [
+        (OPTION_1, 'Good with children'),
+        (OPTION_2, 'Good with other animals'),
+        (OPTION_3, 'Must be leashed at all times'),
 
     ]
-    disposition_description = models.CharField(max_length=100)
+    disposition_1 = models.CharField(max_length=50, choices=PET_DISPOSITION_CHOICES, blank=True)
+    disposition_2 = models.CharField(max_length=50, choices=PET_DISPOSITION_CHOICES, blank=True)
+    disposition_3 = models.CharField(max_length=50, choices=PET_DISPOSITION_CHOICES, blank=True)
 
 
 class Availabilities(models.Model):
+
+    AVAILABLE = 'AV'
+    NOT_AVAILABLE = 'NA'
+    PENDING = 'P'
+    ADOPTED = 'AD'
+
     PET_AVAILABILITY_CHOICES = [
-        ('Available', 'Available'),
-        ('Not Available', 'Not Available'),
-        ('Pending', 'Pending'),
-        ('Adopted', 'Adopted')
+        (AVAILABLE, 'Available'),
+        (NOT_AVAILABLE, 'Not Available'),
+        (PENDING, 'Pending'),
+        (ADOPTED, 'Adopted')
     ]
     availability = models.CharField(max_length=15)
 
@@ -71,15 +94,10 @@ class Pets(models.Model):
     type = models.ForeignKey(Types, on_delete=models.CASCADE)
     breed = models.ForeignKey(Breeds, on_delete=models.CASCADE)
     age = models.ForeignKey(Ages, on_delete=models.CASCADE)
-    disposition_description = models.ForeignKey(Dispositions, on_delete=models.CASCADE)
+    disposition = models.ForeignKey(Dispositions, on_delete=models.CASCADE)
     picture_link = models.CharField(max_length=255)
     availability = models.ForeignKey(Availabilities, on_delete=models.CASCADE)
     location = models.CharField(max_length=255, default='Shelter')
     news = models.CharField(max_length=255, default='')
     last_updated = models.DateTimeField(auto_now=True)
     date_created = models.DateTimeField(auto_now_add=True)
-
-
-# FOR TESTING PURPOSES ONLY
-class Codes(models.Model):
-    code = models.CharField(max_length=8, default='', unique=True)
