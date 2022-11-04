5f15b05bfab44896c3feee1e097c940930d5eebc
Lauren Ruff <ruffl@oregonstate.edu>
2022-11-04 09:02:47 -0700

    uncommented login and signup url links

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', index),            # home page/ news feed
     path('search', index),      # search page
     path('browse', index),      # browse page
-    # path('login', index),      # login page
-    # path('signup', index),      # signup page
+    path('login', index),       # login page
+    path('signup', index),      # signup page
     # path('profile', index),      # profile page
 ]
