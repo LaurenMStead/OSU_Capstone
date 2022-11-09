41114bbd15d4943b1e8f999da62a257f5b743970
nt500 <91642940+nt500@users.noreply.github.com>
2022-11-08 19:05:17 -0800

    Improve appearance of the login form
    Put username field, password field, and the Login button on separate lines, by adding break elements.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -6,10 +6,12 @@ const Login = () => {
       <form action="/login" method="get">
         <label for="username">Username</label>
         <input type="text" placeholder="Enter Username" name="username" required />
-
+        <br></br>
+        <br></br>
         <label for="password">Password</label>
         <input type="password" placeholder="Enter Password" name="password" required />
-
+        <br></br>
+        <br></br>
         <Link to="/"><button type="submit">Login</button></Link>
       </form>
     </div>
