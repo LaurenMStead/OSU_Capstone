9f6ca4c8a164aa1ead65d8fc8f12627e2a2736bb
nt500 <91642940+nt500@users.noreply.github.com>
2022-11-08 19:12:59 -0800

    Improve appearance of the Signup form
    Put the username field, password field, and Signup button on separate lines, by adding break elements.

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -6,10 +6,12 @@ const Signup = () => {
       <form action="/signup" method="post">
         <label for="username">Username</label>
         <input type="text" placeholder="Enter Username" name="username" required />
-
+        <br></br>
+        <br></br>
         <label for="password">Password</label>
         <input type="password" placeholder="Enter Password" name="password" required />
-
+        <br></br>
+        <br></br>
         <Link to="/"><button type="submit">Signup</button></Link>
       </form>
     </div>
