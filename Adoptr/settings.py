317a2af5266580cbe5fe5ebf082bc54ae4b742d1
Lauren Ruff <ruffl@oregonstate.edu>
2022-11-14 11:45:16 -0800

    updated URLs and directories for deployment

--- Adoptr/settings.py
+++ Adoptr/settings.py
@@ -52,10 +52,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Adoptr.urls'
 
+# For deployment - linking react to backend
+FRONTEND_DIR = os.path.join(BASE_DIR, 'ReactFrontend/')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
 
-# for deployment
+# For deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+STATICFILES_DIRS = [
+    os.path.join(FRONTEND_DIR, "build/static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
