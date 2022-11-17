27d391070d3edb9db893b1a17ed2c219a248ca73
nt500 <91642940+nt500@users.noreply.github.com>
2022-11-17 02:39:53 -0800

    Added id fields to the paragraphs
    ID selector is used for CSS styling

--- ReactFrontend/src/components/ProfileCard.jsx
+++ ReactFrontend/src/components/ProfileCard.jsx
@@ -15,13 +15,13 @@ export default function ProfileCard({ pet, selectPet, getNextSpotlightPet }) {
     return (
         <div id="ProfileCard">
             <div className="ProfileCard_image"> pet image here</div>
-            <h1 className="ProfileCard_name">
+            <h1 className="ProfileCard_name id = "ProfileCard_name"">
                 {pet.name}
             </h1>
             <div className="ProfileCard_info">
-                <p className="ProfileCard_age">{pet.age}</p>
-                <p className="ProfileCard_gender">{pet.gender}</p>
-                <p className="ProfileCard_availability">{pet.availability}</p>
+                <p className="ProfileCard_age" id = "ProfileCard_age">{pet.age}</p>
+                <p className="ProfileCard_gender" id = "ProfileCard_gender">{pet.gender}</p>
+                <p className="ProfileCard_availability" id = "ProfileCard_availability">{pet.availability}</p>
             </div>
             {viewBrowseFeature()}
         </div>
