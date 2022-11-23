0f11c07eb5cc8bf2c168a9d955219102c1edaf2a
Lauren Ruff <ruffl@oregonstate.edu>
2022-11-22 17:49:01 -0800

    created a footer, needs some styling help

---
+++ ReactFrontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <footer>
+            <table className="footer">
+                <tr>
+                    <td className="site_title">
+                        ADOPTR : a dating-style webapp for animal adoption
+                    </td>
+                    <td className="project_title">
+                        Oregon State University - Capstone Project - Fall 2022
+                    </td>
+                    <td className="authors">
+                        <tr>Sebastian Espinas</tr>
+                        <tr>Shawn Kim</tr>
+                        <tr>Lauren Ruff</tr>
+                        <tr>Neil Thayamballi</tr>
+                    </td>
+                </tr>
+
+            </table>
+        </footer>
+    )
+}
+
+export default Footer;
